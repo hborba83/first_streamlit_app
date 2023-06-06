fd7cc5897a0678f8059a49fbe97342f8f8b5c043
hborba83 <135717314+hborba83@users.noreply.github.com>
2023-06-06 11:23:50 -0300

    Update streamlit_app.py
    add new elements

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.text('Breakfast menu...')
+streamlit.header('Breakfast menu...')
+
+streamlit.text('1 - ...')
+streamlit.text('2 - ...')
 
